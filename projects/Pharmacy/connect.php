2fc47322b98a2ca6f90b2625f69d72f79ac1e8d8
juneGithub <75412999+rrokusan@users.noreply.github.com>
2024-04-24 11:42:39 +0800

    new project

---
+++ projects/Pharmacy/connect.php
@@ -0,0 +1,12 @@
+<?php
+$server = "localhost";
+$user = "root";
+$password = "";
+
+$db = "pharmacy";
+
+$conn = mysqli_connect($server, $user, $password, $db);
+
+// if($conn){
+//     echo "Connection established.";
+// }

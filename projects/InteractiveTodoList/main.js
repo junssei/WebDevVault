8ea655a042dfa28bbbad04243c5f118cd638a924
juvncent <junevincentmagsayofernandez@gmail.com>
2024-10-03 11:14:35 +0800

    add task and view task

--- projects/InteractiveTodoList/main.js
+++ projects/InteractiveTodoList/main.js
@@ -3,27 +3,31 @@ let count = 0;
 
 document.getElementById("addBtn").addEventListener("click", addList);
 
+let text = document.getElementById("taskList");
+// console.log(text);
+
 function addList() {
-  let taskTxt = document.getElementById("taskTxt").value;
+  let taskTxt = document.getElementById("taskInput").value;
 
   if (taskTxt != "") {
     // console.log(taskTxt);
-    count++;
     taskArray[count] = {
       id: count,
       text: taskTxt,
-      completed: "completed",
+      completed: 'completed',
     };
-    // let text = "<li>" + taskArray[count].text + "</li>";
-    // document.getElementById("taskList").innerHTML = text;
-    display();
+
+    text.innerHTML += "<li>" + taskArray[count].text + "</li>";
+
+    displayList();
+    count++;
   } else {
     alert("Need to input a task!");
   }
 }
 
-function display() {
-  for (let x = 0; x < taskArray.length; x++) {
-    console.log(taskArray[x].text);
+function displayList(){
+  for (let i = 0; i < taskArray.length; i++) {
+    console.log(taskArray[i]);
   }
 }

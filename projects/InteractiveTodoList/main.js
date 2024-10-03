e0c758cc06616e147bdfe1b741641b0ebaa7803c
juvncent <junevincentmagsayofernandez@gmail.com>
2024-10-03 21:52:31 +0800

    design done and fixed functions

--- projects/InteractiveTodoList/main.js
+++ projects/InteractiveTodoList/main.js
@@ -1,10 +1,21 @@
-let taskArray = [];
-let count = 0;
+let taskArray = [
+  {
+    id: 0,
+    text: "IT313 Activity",
+    completed: false,
+  },
+  {
+    id: 1,
+    text: "Watch Anime",
+    completed: false,
+  },
+];
+
+let count = taskArray.length;
+console.log(count);
+displayList();
 
 document.getElementById("addBtn").addEventListener("click", addList);
-
-let text = document.getElementById("taskList");
-// console.log(text);
 
 function addList() {
   let taskTxt = document.getElementById("taskInput").value;
@@ -14,11 +25,12 @@ function addList() {
     taskArray[count] = {
       id: count,
       text: taskTxt,
-      completed: 'completed',
+      completed: false,
     };
 
-    text.innerHTML += "<li>" + taskArray[count].text + "</li>";
+    // text.innerHTML += '<li id="task'+ taskArray[count].id +'">' + taskArray[count].text + '<button class="deletebtn"> Delete </button></li>';
 
+    // console.log("al(): " + count);
     displayList();
     count++;
   } else {
@@ -26,8 +38,30 @@ function addList() {
   }
 }
 
-function displayList(){
+function displayList() {
+  let text = document.getElementById("taskList");
+  let subText = "";
   for (let i = 0; i < taskArray.length; i++) {
+    subText +=
+      '<li id="task' +
+      taskArray[i].id +
+      '">' +
+      taskArray[i].text +
+      '<button class="deletebtn"> Delete </button></li>';
+    text.innerHTML = subText;
+
+    // console.log(subText);
     console.log(taskArray[i]);
   }
+
+  // console.log("ds(): " + count);
+}
+
+document.getElementsByClassName("deletebtn").forEach((element) => {});
+document
+  .getElementsByClassName("deletebtn")
+  .addEventListener("click", deleteList(this));
+
+function deleteList(test) {
+  //
 }

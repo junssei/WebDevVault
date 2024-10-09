ec89320b072c571ef2e028855caaed13bda64885
jibsyx <junevincentmagsayofernandez@gmail.com>
2024-10-09 22:31:10 +0800

    interface 2

--- projects/InteractiveCalculator/main.js
+++ projects/InteractiveCalculator/main.js
@@ -1,1 +1,43 @@
 //
+let num = document.getElementsByClassName("num");
+let dsply = document.getElementById("display");
+
+function displayCalc() {
+  //
+}
+
+// addEventListener to Number Buttons
+for (let i = 0; i < num.length; i++) {
+  num[i].addEventListener("click", function () {
+    console.log(num[i].textContent);
+    dsply.textContent += num[i].textContent;
+  });
+}
+
+// addEventListener to Arithmetic Buttons
+let arithmetic = document.getElementsByClassName("arithmetic");
+for (let i = 0; i < arithmetic.length; i++) {
+  arithmetic[i].addEventListener("click", function () {
+    console.log(arithmetic[i].textContent);
+    dsply.textContent += arithmetic[i].textContent;
+  });
+}
+
+// addEventListener to buttons to clear display/reset calculation
+let clear = document.getElementById("clear");
+clear.addEventListener("click", function () {
+  dsply.textContent = "";
+});
+
+function addition() {
+  //
+}
+function subtraction() {
+  //
+}
+function multiplication() {
+  //
+}
+function division() {
+  //
+}
